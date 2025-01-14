d8e11ab440352b3d0acd0e4a5e82e877ebd959c8
Wildwurger <taekyung1492@gmail.com>
2025-01-14 12:28:17 +0900

    25-01-14 12:28 로그인필요창의 토큰 검증 추가

--- src/main/java/com/capstone/project/security/JwtSecurityConfig.java
+++ src/main/java/com/capstone/project/security/JwtSecurityConfig.java
@@ -1,13 +1,15 @@
 package com.capstone.project.security;
 
-import com.capstone.project.jwt.JwtFilter;
+
 import com.capstone.project.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
     private final TokenProvider tokenProvider;
